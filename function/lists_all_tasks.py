585a5e5c99e3b76e5e902a30c331e7f358d451eb
LuisGomes18 <luiscgomes0@gmail.com>
2025-10-05 23:46:12 +0100

    ♻️ refactor: Translation of the file and function code to view all tasks

---
+++ function/lists_all_tasks.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from utils.manipulate_task_data import load_tasks
+
+
+def lists_all_tasks():
+    tasks = load_tasks()
+
+    for id_tasks, tasks_info in tasks.items():
+        print(f'\Task: {id_tasks}')
+        for key, value in tasks_info.items():
+            print(f'{key}: {value}')
+
+    print('-' * 30)
