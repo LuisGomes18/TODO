8599529e112472e199fe48ae6e6dd5638b9b760d
LuisGomes18 <luiscgomes0@gmail.com>
2025-10-05 19:43:41 +0100

    ♻️ refactor: Translation of the main code file

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import os
 import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from menus.menu_principal import menu_principal
+from menus.main_menu import main_menu
 
 
 if __name__ == '__main__':
-    menu_principal()
+    main_menu()
