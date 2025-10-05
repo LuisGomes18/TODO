629ba67bf2eba082d429ba8d53fe3cdaa8332fec
LuisGomes18 <luiscgomes0@gmail.com>
2025-10-05 19:42:42 +0100

    ♻️ refactor: Translation of the file and code that handles task information

--- utils/manipulate_task_data.py
+++ utils/manipulate_task_data.py
@@ -6,33 +6,34 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from config.settings import BASE_DIR
 
 
-def carregar_tarefas() -> dict:
+def load_tasks() -> dict | None:
     file_path = os.path.join(BASE_DIR, 'data', 'tarefas.json')
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
             return json.load(file)
     except json.JSONDecodeError:
-        print('Erro: ficheiro JSON corrompido ou mal formatado.')
-        return {}
+        print('Error: JSON file corrupted or malformed.')
+        return None
     except FileNotFoundError:
-        print('Aviso: ficheiro não encontrado, a iniciar com dicionário vazio.')
-        return {}
+        print('Warning: file not found')
+        return None
     except Exception as e:
-        print(f'Erro inesperado ao carregar: {e}')
-        return {}
+        print(f'Unexpected error loading tasks: {e}')
+        return None
 
 
-def salvar_tarefas(data: dict) -> None:
+def save_tasks(data: dict) -> None:
     file_path = os.path.join(BASE_DIR, 'data', 'tarefas.json')
+
     if data is None:
-        print('Erro: Nenhum dado para salvar.')
+        print('Error: task data for saving tasks cannot be null')
         return
     if not isinstance(data, dict):
-        print('Erro: os dados devem ser um dicionário.')
+        print('Error: data of tasks to save tasks must be a dictionary')
         return
+
     try:
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
         with open(file_path, 'w', encoding='utf-8') as file:
             json.dump(data, file, indent=4, ensure_ascii=False)
     except Exception as e:
-        print(f'Erro ao salvar dados: {e}')
+        print(f'Error saving task data: {e}')
