3183c789bda9bcc902ae9e00bd3f23b8ceabec10
LuisGomes18 <luiscgomes0@gmail.com>
2025-10-05 23:41:56 +0100

    ♻️ refactor: Translation of the file and code for the function that removes a task by its ID.

---
+++ function/delete_tasks.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from utils.manipulate_task_data import load_tasks, save_tasks
+
+
+def delete_tasks():
+    tasks = load_tasks()
+
+    id = input('What is the task ID: ')
+    if id is None:
+        print('ID cannot be null')
+        return
+
+    if not isinstance(id, str):
+        print('The ID must be of type string.')
+        return
+
+    if id not in tasks:
+        print('ID not found in the task list')
+        return
+
+    del tasks[id]
+    save_tasks(tasks)
