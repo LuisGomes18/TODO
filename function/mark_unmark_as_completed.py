9c4eeb8640a0949b6d082bd61907e1352dd1c1e4
LuisGomes18 <luiscgomes0@gmail.com>
2025-10-06 03:28:38 +0100

    ✨ feat: Addition of function to mark and unmark tasks as complete

---
+++ function/mark_unmark_as_completed.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from utils.manipulate_task_data import load_tasks, save_tasks
+
+
+def mark_all_tasks_as_complete():
+    tasks = load_tasks()
+    if tasks is None:
+        print('There are no tasks in the task list.')
+        return
+
+    for task_id, task_values in tasks.items():
+        task_values['status'] = 'completed'
+
+    save_tasks(tasks)
+
+def unmark_all_tasks_as_complete():
+    tasks = load_tasks()
+    if tasks is None:
+        print('There are no tasks in the task list.')
+        return
+
+    for task_id, task_values in tasks.items():
+        task_values['status'] = 'pending'
+
+    save_tasks(tasks)
--- menus/secondary_menu.py
+++ menus/secondary_menu.py
@@ -3,26 +3,36 @@ import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from function.purge_all_tasks import purge_all_tasks
+from function.mark_unmark_as_completed import mark_all_tasks_as_complete
+from function.mark_unmark_as_completed import unmark_all_tasks_as_complete
 
 
 def secondary_menu():
-    ACCEPTED_CHOICES = ['1', '99', '0']
+    ACCEPTED_CHOICES = ['1', '2', '3', '99', '0']
 
     while True:
         choice = str(input('''
-1) Purge all task data
-99) Main menu
-0) Exit
+1 - Purge all task data
+2 - Mark all tasks as completed
+3 - Unmark all tasks as completed
+99 - Main menu
+0 - Exit
 -> '''))
         while choice not in ACCEPTED_CHOICES:
             choice = str(input('''
-1) Purge all task data
-99) Main menu
-0) Exit
+1 - Purge all task data
+2 - Mark all tasks as completed
+3 - Unmark all tasks as completed
+99 - Main menu
+0 - Exit
 -> '''))
 
         if choice == '1':
             purge_all_tasks()
+        elif choice == '2':
+            mark_all_tasks_as_complete()
+        elif choice == '3':
+            unmark_all_tasks_as_complete()
         elif choice == '99':
             break
         elif choice == '0':
