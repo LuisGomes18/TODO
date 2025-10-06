6706d832193bcd8f283131409dd506ef254fb091
LuisGomes18 <luiscgomes0@gmail.com>
2025-10-06 03:03:09 +0100

    ✨ feat: Addition of a way to create the task data file in the main file

--- app.py
+++ app.py
@@ -2,8 +2,17 @@ import os
 import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from utils.create_task_info_file import create_task_info_file
 from menus.main_menu import main_menu
 
 
 if __name__ == '__main__':
-    main_menu()
+    try:
+        create_task_info_file()
+        main_menu()
+    except InterruptedError:
+        print('\nExiting the program. Goodbye!')
+        exit(0)
+    except Exception as error:
+        print(f'Erro Inesperado: {error}')
+        exit(1)
